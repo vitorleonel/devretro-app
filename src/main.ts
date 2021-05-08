b9e762321ecfa24389f597224cf0a41937f24916
Vitor Leonel <falecom@vitorleonel.com>
2021-05-08 15:31:14 -0300

    start project

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import router from './router';
+
+import App from './components/App';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
