928779c5f38ce2e9636e1bb11e434786a56c8252
Vitor Leonel <falecom@vitorleonel.com>
2021-05-17 22:48:42 -0300

    start socket connection on layout

--- src/layouts/BoardLayout/BoardLayout.vue
+++ src/layouts/BoardLayout/BoardLayout.vue
@@ -8,14 +8,26 @@
 
 <script>
 import Vue from "vue";
+import SocketIOClient from "socket.io-client";
 
 import BoardHeader from "@/components/BoardHeader";
 
-export default Vue.extend({
+export default {
   name: "BoardLayout",
 
   components: {
     BoardHeader,
   },
-});
+
+  created() {
+    const { id: boardId } = this.$route.params;
+
+    Vue.prototype.$socket = SocketIOClient(
+      `http://localhost:3000/boards-${boardId}`,
+      {
+        query: `boardId=${boardId}`,
+      }
+    );
+  },
+};
 </script>
--- src/views/Board/Board.vue
+++ src/views/Board/Board.vue
@@ -5,25 +5,15 @@
 </template>
 
 <script>
-import Vue from "vue";
 import { mapMutations } from "vuex";
-import SocketIOClient from "socket.io-client";
 
 import BoardLayout from "@/layouts/BoardLayout";
 import Board from "@/components/Board";
 
-export default Vue.extend({
+export default {
   components: {
     BoardLayout,
     Board,
-  },
-
-  created() {
-    const { id: boardId } = this.$route.params;
-
-    this.$socket = SocketIOClient(`http://localhost:3000/boards-${boardId}`, {
-      query: `boardId=${boardId}`,
-    });
   },
 
   mounted() {
@@ -33,5 +23,5 @@ export default Vue.extend({
   methods: {
     ...mapMutations(["setBoard"]),
   },
-});
+};
 </script>
