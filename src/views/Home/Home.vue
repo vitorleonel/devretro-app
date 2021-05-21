3964f674efae8b8c8256c15cb8be30e6db749226
Vitor Leonel <falecom@vitorleonel.com>
2021-05-21 07:10:25 -0300

    create and redirect to board

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <GuestLayout class="justify-center">
-    <form method="post" class="w-full max-w-sm mx-auto" @submit.prevent="">
+    <form
+      method="post"
+      class="w-full max-w-sm mx-auto"
+      @submit.prevent="submitForm"
+    >
       <InputField
         label="Board name:"
         name="boardName"
         placeholder="Your board name"
         v-model="boardName"
+        :disabled="loading"
       />
 
-      <Button class="bg-gray-700 hover:bg-gray-900 text-white"
-        >Create board</Button
+      <Button
+        class="text-white"
+        :class="{
+          'bg-gray-700 hover:bg-gray-900': !loading,
+          'bg-gray-500 cursor-wait': loading,
+        }"
+        :disabled="loading"
+        >{{ !loading ? "Create board" : "Creating board..." }}</Button
       >
     </form>
   </GuestLayout>
@@ -21,6 +32,8 @@ import GuestLayout from "@/layouts/GuestLayout";
 import InputField from "@/components/InputField";
 import Button from "@/components/Button";
 
+import HttpService from "@/services/http";
+
 export default {
   components: {
     GuestLayout,
@@ -30,8 +43,33 @@ export default {
 
   data() {
     return {
+      loading: false,
       boardName: "",
     };
+  },
+
+  methods: {
+    async submitForm() {
+      const boardName = this.boardName.trim();
+
+      if (!boardName) {
+        return;
+      }
+
+      this.loading = false;
+
+      try {
+        const {
+          data: { board },
+        } = await HttpService.post("/boards", {
+          name: boardName,
+        });
+
+        this.$router.push({ name: "Board", params: { id: board._id } });
+      } catch (error) {
+        this.loading = false;
+      }
+    },
   },
 };
 </script>
