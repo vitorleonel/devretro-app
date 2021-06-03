7ac5add7faf6580ba25c42c802f5548f39fd04d1
Vitor Leonel <falecom@vitorleonel.com>
2021-06-02 23:58:57 -0300

    add user photo

--- src/components/BoardHeader/BoardHeader.vue
+++ src/components/BoardHeader/BoardHeader.vue
@@ -1,43 +1,29 @@
 <template>
   <header
     class="w-full h-16 bg-white px-6 border-b flex items-center justify-between relative"
+    v-if="user && board"
   >
     <div class="text-xl">Dev<span class="font-bold">Retro</span></div>
 
     <h1 class="w-full text-lg text-center absolute -ml-6">
-      {{ !loading ? board.name : "Loading..." }}
+      {{ board.name }}
     </h1>
 
-    <div v-show="expireIn">Expires {{ expireIn }}</div>
+    <div class="w-8 h-8 bg-gray-300 rounded-full">
+      <img :src="user.photo" :alt="user.name" class="rounded-full" />
+    </div>
   </header>
 </template>
 
 <script>
-import Vue from "vue";
 import { mapState } from "vuex";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-
-dayjs.extend(relativeTime);
-
-export default Vue.extend({
+export default {
   computed: {
     ...mapState({
-      loading: (state) => state.board.loading,
+      user: (state) => state.auth.data,
       board: (state) => state.board.data,
     }),
-
-    expireIn() {
-      if (!this.board) {
-        return;
-      }
-
-      const boardExpiredIn = dayjs(this.board.createdAt).add(2, "day");
-      const current = dayjs();
-
-      return current.to(boardExpiredIn);
-    },
   },
-});
+};
 </script>
