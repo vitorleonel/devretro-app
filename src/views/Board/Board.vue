e1d1eb91cdc2259b9ef7428498fd47b56bedcdd7
Vitor Leonel <falecom@vitorleonel.com>
2021-06-02 23:59:30 -0300

    add loading

--- src/views/Board/Board.vue
+++ src/views/Board/Board.vue
@@ -1,19 +1,29 @@
 <template>
   <BoardLayout>
-    <Board />
+    <Loading v-if="boardLoading" />
+    <Board v-else />
   </BoardLayout>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapState, mapMutations } from "vuex";
 
 import BoardLayout from "@/layouts/BoardLayout";
+
+import Loading from "@/components/Loading";
 import Board from "@/components/Board";
 
 export default {
   components: {
     BoardLayout,
+    Loading,
     Board,
+  },
+
+  computed: {
+    ...mapState({
+      boardLoading: (state) => state.board.loading,
+    }),
   },
 
   mounted() {
