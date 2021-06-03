bb480ae7c699a61f9c1671b7791a6d7a744f587d
Vitor Leonel <falecom@vitorleonel.com>
2021-06-03 00:00:03 -0300

    loading and firebase configuration

--- src/components/App/App.vue
+++ src/components/App/App.vue
@@ -1,3 +1,47 @@
 <template>
-  <router-view></router-view>
+  <SignIn v-if="!isAuthenticated" />
+  <router-view v-else></router-view>
 </template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/analytics";
+
+import SignIn from "@/components/SignIn";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_FIREBASE_APPID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENTID,
+};
+
+export default {
+  components: {
+    SignIn,
+  },
+
+  computed: {
+    ...mapGetters(["isAuthenticated"]),
+  },
+
+  created() {
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  },
+
+  mounted() {
+    firebase.auth().onAuthStateChanged(this.authUser);
+  },
+
+  methods: {
+    ...mapActions(["authUser"]),
+  },
+};
+</script>
